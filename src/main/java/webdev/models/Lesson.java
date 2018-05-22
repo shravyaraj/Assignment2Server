fcbe404155b398038d7fb2b64f3f532465aad7b4
shravyaraj <38574502+shravyaraj@users.noreply.github.com>
2018-05-22 13:18:10 -0400

    Added lesson model,repository and service

--- src/main/java/webdev/models/Lesson.java
+++ src/main/java/webdev/models/Lesson.java
@@ -1,26 +1,22 @@
 package webdev.models;
 
-import java.util.List;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
-public class Module {
+public class Lesson {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
 	private String title;
-	@OneToMany(mappedBy="module")
-	private List<Lesson> lessons;
 	@ManyToOne
 	@JsonIgnore
-	private Course course;
+	private Module module;
 	public int getId() {
 		return id;
 	}
@@ -33,10 +29,10 @@ public class Module {
 	public void setTitle(String title) {
 		this.title = title;
 	}
-	public Course getCourse() {
-		return course;
+	public Module getModule() {
+		return module;
 	}
-	public void setCourse(Course course) {
-		this.course = course;
+	public void setModule(Module module) {
+		this.module = module;
 	}
 }
